a2e3c91cd5cd77cdd37fc661147a508b155f0f6f
Henry Müssemann <hm@freezingdata.de>
2023-03-28 14:45:03 +0200

    Updating setup.py to automatically detect version

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 import setuptools
+import os
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+def get_version():
+    """
+    Return the version string based on the current Git tag, or a default
+    value of "0.0.1" if the tag cannot be determined.
+    """
+    if "GITHUB_REF" in os.environ:
+        tag_ref = os.environ["GITHUB_REF"]
+        if tag_ref.startswith("refs/tags/"):
+            return tag_ref[len("refs/tags/"):]
+    return "0.0.1"
+
+def get_readme_long():
+    with open("README.md", "r") as f:
+        return f.read()
 
 setuptools.setup(
     name="selenium-print",
     packages=setuptools.find_packages(),
-    version="1.0.0",
+    version=get_version(),
     license="MIT",
     description="A minimal printing utility based on selenium and chrome that is able to render any HTML to PDF.",
     author="Henry Müssemann",
     author_email="henry@muessemann.de",
     url="https://github.com/bubblegumsoldier/selenium-print",
-    long_description=long_description,
+    long_description=get_readme_long(),
     long_description_content_type="text/markdown",
     keywords=["print", "pdf", "converter", "selenium"],
     install_requires=['selenium'],
